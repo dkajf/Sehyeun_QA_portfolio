7b9c964e56cf3c04bb70d3a923f747143261448d
dkajf <oshe4071@gmail.com>
2026-03-12 13:36:02 +0900

    fix boj 1920 sol #3

--- March/Week_2/Code_study/Practice_01/Solution.cpp
+++ March/Week_2/Code_study/Practice_01/Solution.cpp
@@ -6,12 +6,15 @@ using namespace std;
 
 int main(){
 
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int N, M;
     vector<int> A;
 
     cin >> N;
 
-    for(int i;i<N;i++){
+    for(int i=0;i<N;i++){
         int x;
         cin >> x;
         A.push_back(x);
